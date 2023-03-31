c5e1f0c62e625750910244589cc396e11f064685
Bialri <kardasov96@mail.ru>
2023-03-31 17:45:37 +0300

    remove tests app

--- Onboarding/settings.py
+++ Onboarding/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'welcomejorney.apps.WelcomejorneyConfig',
     'notifications.apps.NotificationsConfig',
-    'tests.apps.TestsConfig',
 ]
 
 MIDDLEWARE = [
--- tests/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FillView
-
-urlpatterns = [
-    path('fill/',FillView.as_view())
-]
