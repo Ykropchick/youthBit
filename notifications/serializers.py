3d09b298622910b25518b7dfabeacd9c9f3840e9
Bialri <kardasov96@mail.ru>
2023-04-27 21:40:50 +0300

    Views and serializers test for notification app

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -6,18 +6,28 @@ user_model = get_user_model()
 
 
 class CustomUserInfoSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = user_model
         fields = ('pk', 'firstname', 'lastname')
 
 
 class NotificationSerializer(serializers.ModelSerializer):
-    to = serializers.PrimaryKeyRelatedField(queryset=user_model.objects.all())
-    sender = serializers.PrimaryKeyRelatedField(queryset=user_model.objects.all())
+    to = CustomUserInfoSerializer(many=False, read_only=True)
+    to_id = serializers.IntegerField(write_only=True)
+    sender = CustomUserInfoSerializer(many=False, read_only=True)
+    sender_id = serializers.IntegerField(write_only=True)
     date = serializers.DateTimeField(read_only=True)
-    is_read = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Notification
-        fields = ('pk', 'title', 'description', 'to', 'sender', 'date', 'is_read')
+        fields = ('pk', 'title', 'description', 'to', 'to_id', 'sender', 'sender_id', 'date', 'is_read')
+
+    def update(self, instance, validated_data):
+        validated_data['to'] = user_model.objects.get(pk=validated_data.pop('to_id'))
+        validated_data['sender'] = user_model.objects.get(pk=validated_data.pop('sender_id'))
+        return super().update(instance, validated_data)
+
+    def create(self, validated_data):
+        validated_data['to'] = user_model.objects.get(pk=validated_data.pop('to_id'))
+        validated_data['sender'] = user_model.objects.get(pk=validated_data.pop('sender_id'))
+        return super().create(validated_data)
--- notifications/urls.py
+++ notifications/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 
 from .views import (NotificationListView, NotificationCreateView,
-                    NotificationUpdateStatusView)
+                    NotificationUpdateView)
 
 
 urlpatterns = [
-    path('notifications/', NotificationListView.as_view()),
-    path('notifications/update/<int:pk>/', NotificationUpdateStatusView.as_view()),
-    path('notifications/create/', NotificationCreateView.as_view()),
+    path('notifications/', NotificationListView.as_view(), name='get_list_notifications'),
+    path('notifications/update/<int:pk>/', NotificationUpdateView.as_view(),
+         name='update_notification'),
+    path('notifications/create/', NotificationCreateView.as_view(), name='create_notification'),
 ]
